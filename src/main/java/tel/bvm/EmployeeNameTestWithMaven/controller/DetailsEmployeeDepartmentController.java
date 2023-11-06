69bd6b24b5f2726ca03028bac0c8b63fba65da23
bkvdm <vadim@991199.ru>
2023-11-06 17:21:25 +0300

    Домашняя работа №24 "Библиотеки". Ссылка на задание: https://my.sky.pro/student-cabinet/stream-lesson/75302/homework-requirements

--- src/main/java/tel/bvm/EmployeeNameTestWithMaven/controller/DetailsEmployeeDepartmentController.java
+++ src/main/java/tel/bvm/EmployeeNameTestWithMaven/controller/DetailsEmployeeDepartmentController.java
@@ -1,4 +1,46 @@
+//package tel.bvm.EmployeeNameTestWithMaven.controller;
+//
+//public class DetailsEmployeeDepartmentController {
+//}
+
 package tel.bvm.EmployeeNameTestWithMaven.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import tel.bvm.EmployeeNameTestWithMaven.scheme.Employee;
+import tel.bvm.EmployeeNameTestWithMaven.service.DetailsEmployeeDepartmentService;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/employee/details")
 public class DetailsEmployeeDepartmentController {
+    private final DetailsEmployeeDepartmentService detailsEmployeeDepartmentService;
+
+    public DetailsEmployeeDepartmentController(DetailsEmployeeDepartmentService detailsEmployeeDepartmentService) {
+        this.detailsEmployeeDepartmentService = detailsEmployeeDepartmentService;
+    }
+
+    @GetMapping
+    public Map<String, Employee> allEmployeeInfo() {
+        return detailsEmployeeDepartmentService.allEmployeeInfo();
+    }
+
+    @GetMapping("/departments/max-salary")
+    public Employee maxWageDepartment(@RequestParam(value = "department", required = false) Integer departmentNumber) {
+        return detailsEmployeeDepartmentService.maxWageDepartment(departmentNumber);
+    }
+
+    @GetMapping("/departments/min-salary")
+    public Employee minWageDepartment(@RequestParam(value = "department", required = false) Integer departmentNumber) {
+        return detailsEmployeeDepartmentService.minWageDepartment(departmentNumber);
+    }
+
+    @GetMapping("/departments/all")
+    public String employeeDepartment(@RequestParam(value = "department", required = false) Integer departmentNumber) {
+        return detailsEmployeeDepartmentService.allEmployeeGroup(departmentNumber);
+    }
 }
+
--- src/main/java/tel/bvm/EmployeeNameTestWithMaven/service/DetailsEmployeeDepartmentService.java
+++ src/main/java/tel/bvm/EmployeeNameTestWithMaven/service/DetailsEmployeeDepartmentService.java
@@ -1,4 +1,19 @@
+//package tel.bvm.EmployeeNameTestWithMaven.service;
+//
+//public interface DetailsEmployeeDepartmentService {
+//}
 package tel.bvm.EmployeeNameTestWithMaven.service;
 
+import tel.bvm.EmployeeNameTestWithMaven.scheme.Employee;
+
+import java.util.Map;
+
 public interface DetailsEmployeeDepartmentService {
+    Employee maxWageDepartment(Integer departmentNumber);
+
+    Map<String, Employee> allEmployeeInfo();
+
+    Employee minWageDepartment(Integer departmentNumber);
+
+    String allEmployeeGroup(Integer department);
 }
